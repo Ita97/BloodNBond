dc1d284dc9180461a02fb890cf14bced329af993
ita97 <lorenzo.ita@hotmail.com>
2017-12-28 15:49:49 +0100

    template inventory

--- Inventory.cpp
+++ Inventory.cpp
@@ -10,7 +10,6 @@ bool Inventory<T>::getElement(T el){
         return false;
     element.push_back(el);
     return true;
-
 }
 
 template <typename T>
@@ -26,4 +25,26 @@ bool Inventory<T>::trowElement(T el) {
     }
 
 
+}
+template <typename T>
+void Inventory<T>::openWindow() {
+    std::string elementName=0;
+    int firstLine=0;
+    inventoryWindow.setSize(sf::Vector2f(100,70));
+    inventoryWindow.setPosition(0,0);
+    inventoryWindow.setFillColor(sf::Color(211,211,211,120)); //silver color
+    inventoryWindow.setOutlineThickness(5);
+    inventoryWindow.setOutlineColor(sf::Color::Black);
+    isOpen=true;
+
+    if(sf::Mouse::getPosition().x>inventoryWindow.getOrigin().x&& sf::Mouse::getPosition().x<inventoryWindow.getSize().x+inventoryWindow.getOrigin().x)
+        if (sf::Mouse::getPosition().y>inventoryWindow.getOrigin().y&& sf::Mouse::getPosition().y<inventoryWindow.getSize().y+inventoryWindow.getOrigin().y)
+            firstLine=firstLine+rollCounter;
+
+    for(int i=firstLine;i<9+firstLine;i++){
+        elementName=elementName+element[i].getName()+"/n";//todo finish: find a way to insert a string in stl
+    }
+
+    text.setString(elementName);
+    elementName=0;
 }
