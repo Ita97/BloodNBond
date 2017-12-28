a4cf4c6653a573cdc75080a81ac1e2ded0c12990
ita97 <lorenzo.ita@hotmail.com>
2017-12-28 15:46:21 +0100

    template inventory

--- Inventory.h
+++ Inventory.h
@@ -8,6 +8,7 @@
 
 #include <vector>
 #include <algorithm>
+#include <SFML/Graphics.hpp>
 
 template <typename T>
 
@@ -15,20 +16,34 @@ class Inventory {
 private:
     int size;
     std::vector<T> element;
+    sf::RectangleShape inventoryWindow;
+    sf::Font invFont;
+    sf::Text text;
+    int rollCounter;
+    bool isOpen;
     //todo se vuoi aggiungi categoria..
 
 public:
-    explicit Inventory(int size=15, std:: vector<T> chest={}):
-            size(size), element(chest){
+    explicit Inventory(int size=15, std:: vector<T> chest={}): size(size), element(chest){
+        rollCounter=0;
+        isOpen=false;
+        invFont.loadFromFile("/home/ita/CLionProjects/BloodBond/Font/BeautyDemo.ttf");
+        text.setFont(invFont);
+        text.setPosition(inventoryWindow.getPosition().x+10,inventoryWindow.getPosition().y+5);
+        text.setFillColor(sf::Color::Black);
+        text.setCharacterSize(10);
     }
     int getSize() const{
         return size;
     }
     void setSize(int s){
-        Inventory::size=s;
+        size=s;
     }
     bool getElement(T el);
     bool trowElement(T el);
+
+    void openWindow();
+
 };
 
 
