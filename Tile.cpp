aa75de5d8ff7dea16231e3222a57e369678ca0d9
Ita97 <lorenzo.ita@hotmail.com>
2018-03-01 18:49:14 +0100

    Update Tile.cpp

--- Tile.cpp
+++ Tile.cpp
@@ -1,5 +1,27 @@
 //
 // Created by ita on 10/02/18.
 //
-
 #include "Tile.h"
+
+Tile::Tile(TileType type) {
+    obstacle= nullptr;
+    size.x=256;
+    size.y=153,33;
+    switch(type){
+        case TileType::floor:
+            texture.loadFromFile("./texture/map/tile/brick.png");
+            break;
+        case TileType::grass:
+            texture.loadFromFile("./texture/map/tile/grass.png");
+            break;
+        case TileType:: wall:
+            texture.loadFromFile("./texture/map/tile/wall.png");
+            break;
+        case TileType ::land:
+            texture.loadFromFile("./texture/map/tile/dirt.png");
+            break;
+        case TileType ::dark:
+            texture.loadFromFile("./texture/map/tile/blackGrass.jpg");
+    }
+    sprite.setTexture(texture);
+}
