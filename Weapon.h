057c7412da8857f5f30fbdb45dd7fb1cb5dbb47d
Ita97 <lorenzo.ita@hotmail.com>
2018-03-01 18:52:03 +0100

    Update Weapon.h
    Weapon sprite added

--- Weapon.h
+++ Weapon.h
@@ -9,7 +9,6 @@
 #include <SFML/System.hpp>
 #include "Window.h"
 
-
 class Weapon {
 protected:
     std::string name;
@@ -18,26 +17,32 @@ protected:
     std::string description;
     sf::Vector2f range;
     sf::Clock weaponClock;
-    sf::Texture texture;
+    sf::Texture texture,weaponTxt;
     sf::Vector2i frameDistance,textureSize;
     int frameCount;
-    bool hit;
+    bool ready;
+    sf::Sprite sprite;
 
 public:
     explicit Weapon()= default;
     virtual ~Weapon()= default;
 
     virtual sf::Vector2f getCollisionArea()=0;
-   const std::string &getName() const{
+
+    const std::string &getName() const{
         return name;
     }
+
     virtual void use()=0;
-    virtual int startAttack(sf::Vector2f detPosition, int direction)=0;
+
+    virtual void startAttack(sf::Vector2f detPosition, int direction)=0;
+
     bool checkCollision(sf::Vector2f target, sf::Vector2f collisionArea,sf::Vector2f focus, sf::Vector2f range=sf::Vector2f(0,0));
 
     int getStrength() const {
         return strength;
     }
+
     void setStrength(int strength) {
         this->strength = strength;
     }
@@ -52,17 +57,27 @@ public:
 
     virtual Weapon* clone()=0;
 
-    virtual void Render(Window &l_window)=0;
+    virtual void Render(sf::RenderWindow &l_window)=0;
 
-    virtual const sf::Texture& getTexture();
-    void getTextureDetails(sf::Vector2i& size, sf::Vector2i& distance, int& frameNumber){
-        size=textureSize;
-        distance=frameDistance;
-        frameNumber=frameCount;
+    const sf::Texture& getTexture();
+
+    sf::Sprite& getSprite(){
+        return sprite;
     }
+
+    void getTextureDetails(sf::Vector2i& size, sf::Vector2i& distance, int& frameNumber);
+
     virtual sf::Vector2f getPosition()=0;
+
     sf::Vector2f getRange(){
         return range;
+    }
+
+    void disable(){
+        ready=false;
+    }
+    bool isReady(){
+        return ready;
     }
 };
 
