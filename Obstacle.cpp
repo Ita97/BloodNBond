c9c0b39628cc3ea5760a707939238ef8a93f100e
Ita97 <lorenzo.ita@hotmail.com>
2018-03-01 18:42:40 +0100

    Update Obstacle.cpp
    action() added

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -11,17 +11,18 @@ Obstacle::Obstacle(ObstacleType type,int select):type(type){
     unlockable=false;
     texture.setSmooth(true);
     lock=keyType::null;
+    action="";
     switch(type){
         case ObstacleType::deadTree:
-            texture.loadFromFile("/home/ita/CLionProjects/BloodBond/texture/obstacle/dead_tree.png");
+            texture.loadFromFile("./texture/map/obstacle/dead_tree.png");
             sprite.scale(1.3,1.3);
             sprite.setTextureRect(sf::IntRect(select*128,0,134,250));
-            sprite.setOrigin(63,188);
-            area=sf::Vector2f(12,6);
+            sprite.setOrigin(63,180);
+            area=sf::Vector2f(12,10);
             collisionArea=sf::Vector2f(134,250);
             break;
         case ObstacleType::tree:
-            texture.loadFromFile("/home/ita/CLionProjects/BloodBond/texture/obstacle/tree.png");
+            texture.loadFromFile("./texture/map/obstacle/tree.png");
             sprite.scale(1.5,1.5);
             sprite.setTextureRect(sf::IntRect(select*132,0,134,164));//134,164
             sprite.setOrigin(65,120);
@@ -30,13 +31,13 @@ Obstacle::Obstacle(ObstacleType type,int select):type(type){
             break;
         case ObstacleType ::grave:
             if(select==0){//cross
-                texture.loadFromFile("/home/ita/CLionProjects/BloodBond/texture/obstacle/cross.png");
+                texture.loadFromFile("./texture/map/obstacle/cross.png");
                 sprite.scale(0.22,0.2);
                 sprite.setTextureRect(sf::IntRect(250,100,550,500));
-                sprite.setOrigin(150,275);
+                sprite.setOrigin(150,280);
                 sprite.setColor(sf::Color(150,150,150));
                 area=sf::Vector2f(30,25);
-                collisionArea=sf::Vector2f(130,200);
+                collisionArea=sf::Vector2f(130,150);
             }
             else if(select==1){}//grave
             else if(select==2){}//obelisc
@@ -44,7 +45,7 @@ Obstacle::Obstacle(ObstacleType type,int select):type(type){
         break;
         case ObstacleType ::forniture:
             if(select==0){//table
-                texture.loadFromFile("/home/ita/CLionProjects/BloodBond/texture/obstacle/table.png");
+                texture.loadFromFile("./texture/map/obstacle/table.png");
                 sprite.scale(0.4,0.4);
                 sprite.setOrigin(texture.getSize().x/2,texture.getSize().y/2+30);
                 area=sf::Vector2f(100,40);
@@ -54,28 +55,45 @@ Obstacle::Obstacle(ObstacleType type,int select):type(type){
         case ObstacleType :: woodpile:
             break;
 
-
-
         case ObstacleType ::building:
 
             if(select==0){//mansion_close
                 unlockable=true;
                 lock=keyType ::skeleton;
-                texture.loadFromFile("/home/ita/CLionProjects/BloodBond/texture/obstacle/mansion_close.png");
+                texture.loadFromFile("./texture/map/obstacle/mansion_close.png");
 
                 sprite.setOrigin(texture.getSize().x/2-115,texture.getSize().y-160);
                 sprite.scale(1.3,1);
                 area=sf::Vector2f(100,30);
                 collisionArea=sf::Vector2f(100,100);
+                action="Door unlocked!";
             }
             else if(select==1){//mansion_open
                 open=true;
-                texture.loadFromFile("/home/ita/CLionProjects/BloodBond/texture/obstacle/mansion_open.png");
+                texture.loadFromFile("./texture/map/obstacle/mansion_open.png");
                 sprite.scale(1.3,1);
                 sprite.setOrigin(texture.getSize().x/2-115,texture.getSize().y-160);
                 area=sf::Vector2f(0,0);
                 collisionArea=sf::Vector2f(0,0);
             }
+            break;
+        case ObstacleType ::box:
+            if(select==0){//close
+                unlockable=true;
+                texture.loadFromFile("./texture/map/obstacle/box.png");
+                sprite.setTextureRect(sf::IntRect(0,0,100,100));
+                sprite.setOrigin(60,50);
+                area=sf::Vector2f(40,30);
+                collisionArea=sf::Vector2f(65,50);
+            }else{//open
+                open=true;
+                texture.loadFromFile("./texture/map/obstacle/box.png");
+                sprite.setTextureRect(sf::IntRect(118,0,100,100));
+                sprite.setOrigin(60,50);
+                area=sf::Vector2f(40,30);
+                collisionArea=sf::Vector2f(65,50);
+            }
+
             break;
         default:
             sprite.setTextureRect(sf::IntRect(0,0,0,0));
@@ -84,6 +102,6 @@ Obstacle::Obstacle(ObstacleType type,int select):type(type){
     }
         sprite.setTexture(texture);
 }
-void Obstacle::Render(Window &window) {
-    window.Draw(sprite);
+void Obstacle::Render(sf::RenderWindow &window) {
+    window.draw(sprite);
 }
