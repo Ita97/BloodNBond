b1bd209d01bd35a2719d3d096b26a54e1c7b061a
Ita97 <lorenzo.ita@hotmail.com>
2018-03-01 18:03:14 +0100

    Update Detective.h
    Observer function released

--- Detective.h
+++ Detective.h
@@ -14,102 +14,163 @@
 #include "Key.h"
 #include "Message.h"
 #include "Obstacle.h"
+#include "Observer.h"
 #include <SFML/Graphics.hpp>
 
 
-class Detective: virtual public Character {
+class Detective: virtual public Character, virtual public Observer {
 public:
-    Detective( const std::string& det,int h, int sp, int ap, int x, int y, Weapon* w);
+    Detective(const std::string &det, int h, int sp, int x, int y, Weapon *w);
+
     Detective(const Detective &original);
-    Detective& operator =(const Detective &right);
+
+    Detective &operator=(const Detective &right);
 
     void move() override;
+
     void attack();
-    void use(Window& window);
-    void update(){
-        key=keyType ::null;
+
+    void use(sf::RenderWindow &window);
+
+    void reload() {
+        key = keyType::null;
+        HP.setSize(sf::Vector2f(115*healtPoint/maxHP,HP.getSize().y));
+        SP.setSize(sf::Vector2f(115*sanityPoint/maxSP,SP.getSize().y));
     }
-    int getSanity() const{
+
+    int getSanity() const {
         return sanityPoint;
     }
-    void setSanity(int sp){
-        sanityPoint+=sp;
-    };
-    int getAbilityPoint() const{
-        return abilityPoint;
+
+    void setSanity(int sp) {
+        if(sp>0) {
+            sanityPoint += sp;
+            if(sanityPoint>maxSP)
+                sanityPoint=maxSP;
+        }
     }
-    void setAbilityPoint(int ap){
-        abilityPoint=ap;
+
+    void getMad(int sp) {
+        if(sp>0) {
+            sanityPoint -= sp;
+            if(sanityPoint<0)
+                sanityPoint=0;
+        }
     }
+
+    void heal(int value){
+        if(value>0) {
+            healtPoint += value;
+            if(healtPoint>maxHP)
+                healtPoint=maxHP;
+        }
+    }
+
     void getMedicine(Medicine &med);
-    void openMedikit(){
+
+    void openMedikit() {
         medikit.openWindow();
     }
-    void closeMedikit(){
+
+    void closeMedikit() {
         medikit.closeWindow();
     }
+
     void getMessage(Message &note);
-    void openNotebook(){
+
+    void openNotebook() {
         notebook.openWindow();
     }
-    void closeNotebook(){
+
+    void closeNotebook() {
         notebook.closeWindow();
     }
-    void getKey(Key& key);
-    void openKeychain(){
+
+    void getKey(Key &key);
+
+    void openKeychain() {
         keychain.openWindow();
     };
-    void closeKeychain(){
+
+    void closeKeychain() {
         keychain.closeWindow();
     }
-    void useMedicine(Medicine& medication);
 
-    void setWeapon(Weapon* weapon){
-        this->weapon=weapon;
+    void useMedicine(Medicine &medication);
+
+    void setWeapon(Weapon *weapon) {
+        throwWeapon();
+        this->weapon = weapon;
         sprite.setTexture(weapon->getTexture());
     }
-    void throwWeapon(){
-        sprite.setTexture(texture);
-        weapon= nullptr;
-    }
-    void getTorch(){
-        torch=true;
-    }
-    bool haveTorch(){
-        return torch;
-    }
-    void Render(Window& l_window) override;
-    void RenderInventory(Window& l_window);
-    bool isFighting(){
-        return fight;
-    }
-    const Weapon& getWeapon(){
+
+
+
+
+    void Render(sf::RenderWindow &l_window) override;
+
+    void RenderInventory(sf::RenderWindow &l_window);
+
+    const Weapon &getWeapon() {
         return *weapon;
     }
 
 
-    int getAttackPower(){
-        if(weapon!= nullptr)
+    int getAttackPower() {
+        if (weapon != nullptr)
             return weapon->getStrength();
         return 1;
     }
+
     sf::Vector2f getFeetPosition() override;
-    void interact(Obstacle* obs);
+
+    void interact(Obstacle *obs);
+    void reset(){
+        throwWeapon();
+        medikit.reset();
+        keychain.reset();
+        notebook.reset();
+        healtPoint=maxHP;
+        sanityPoint=maxSP;
+    }
+    void throwWeapon() {
+        sprite.setTexture(texture);
+        weapon= nullptr;
+    }
+
+    void update() override{
+        level++;
+        if(level==5){
+            star1.setTexture(red_star);
+            star1.setOrigin(red_star.getSize().x/2,red_star.getSize().y/2);
+            star1.setScale(0.175,0.165);
+        }
+        else if(level==10){
+            star2.setTexture(blue_star);
+            star2.setOrigin(blue_star.getSize().x/2,blue_star.getSize().y/2);
+            star2.setScale(0.3,0.305);
+        }else if(level==20){
+            star3.setTexture(yellow_star);
+            star3.setOrigin(yellow_star.getSize().x/2,yellow_star.getSize().y/2);
+            star3.setScale(0.16,0.159);
+        }
+
+    }
+    void attach() override{}
+    void detach() override{}
+
 private:
+    int level;
     std::string name;
-    int sanityPoint;
+    int sanityPoint, maxHP, maxSP;
     int abilityPoint;
-    bool torch, fight;
     float attackFrame;
     Inventory<Medicine> medikit;
     Inventory<Key> keychain;
     Inventory<Message> notebook;
     keyType key;
-
-
-
-
+    sf::Texture state_txt,void_star,blue_star,red_star,yellow_star;
+    sf::Sprite state_bar,star1,star2,star3;
+    sf::RectangleShape HP,SP;
 };
-
-
 #endif //BLOODBOND_DETECTIVE_H
