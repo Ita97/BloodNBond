f70cc386c83f8bb23db84ea7751d2107c5330601
Ita97 <lorenzo.ita@hotmail.com>
2018-03-01 18:43:52 +0100

    Update Obstacle.cpp
    action() added

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -4,104 +4,64 @@
 
 #include "Obstacle.h"
 
-Obstacle::Obstacle(ObstacleType type,int select):type(type){
-    if(select>3)
-        select=0;
-    open=false;
-    unlockable=false;
-    texture.setSmooth(true);
-    lock=keyType::null;
-    action="";
-    switch(type){
-        case ObstacleType::deadTree:
-            texture.loadFromFile("./texture/map/obstacle/dead_tree.png");
-            sprite.scale(1.3,1.3);
-            sprite.setTextureRect(sf::IntRect(select*128,0,134,250));
-            sprite.setOrigin(63,180);
-            area=sf::Vector2f(12,10);
-            collisionArea=sf::Vector2f(134,250);
-            break;
-        case ObstacleType::tree:
-            texture.loadFromFile("./texture/map/obstacle/tree.png");
-            sprite.scale(1.5,1.5);
-            sprite.setTextureRect(sf::IntRect(select*132,0,134,164));//134,164
-            sprite.setOrigin(65,120);
-            area=sf::Vector2f(30,30);
-            collisionArea=sf::Vector2f(134,164);
-            break;
-        case ObstacleType ::grave:
-            if(select==0){//cross
-                texture.loadFromFile("./texture/map/obstacle/cross.png");
-                sprite.scale(0.22,0.2);
-                sprite.setTextureRect(sf::IntRect(250,100,550,500));
-                sprite.setOrigin(150,280);
-                sprite.setColor(sf::Color(150,150,150));
-                area=sf::Vector2f(30,25);
-                collisionArea=sf::Vector2f(130,150);
-            }
-            else if(select==1){}//grave
-            else if(select==2){}//obelisc
-            else if(select==3){}//tombwoof
-        break;
-        case ObstacleType ::forniture:
-            if(select==0){//table
-                texture.loadFromFile("./texture/map/obstacle/table.png");
-                sprite.scale(0.4,0.4);
-                sprite.setOrigin(texture.getSize().x/2,texture.getSize().y/2+30);
-                area=sf::Vector2f(100,40);
-                collisionArea=sf::Vector2f(200,300);
-            }
-            break;
-        case ObstacleType :: woodpile:
-            break;
+#include <SFML/Graphics.hpp>
+#include "Key.h"
 
-        case ObstacleType ::building:
+enum class ObstacleType{deadTree, tree, grave, forniture, woodpile, building, box, null};
 
-            if(select==0){//mansion_close
-                unlockable=true;
-                lock=keyType ::skeleton;
-                texture.loadFromFile("./texture/map/obstacle/mansion_close.png");
+class Obstacle {
+private:
+    sf::Vector2f position;
+    sf::Vector2f area,collisionArea;
+    sf::Texture texture;
+    sf::Sprite sprite;
+    std::string action;
+    ObstacleType type;
+    keyType lock;
+    bool unlockable,open;
 
-                sprite.setOrigin(texture.getSize().x/2-115,texture.getSize().y-160);
-                sprite.scale(1.3,1);
-                area=sf::Vector2f(100,30);
-                collisionArea=sf::Vector2f(100,100);
-                action="Door unlocked!";
-            }
-            else if(select==1){//mansion_open
-                open=true;
-                texture.loadFromFile("./texture/map/obstacle/mansion_open.png");
-                sprite.scale(1.3,1);
-                sprite.setOrigin(texture.getSize().x/2-115,texture.getSize().y-160);
-                area=sf::Vector2f(0,0);
-                collisionArea=sf::Vector2f(0,0);
-            }
-            break;
-        case ObstacleType ::box:
-            if(select==0){//close
-                unlockable=true;
-                texture.loadFromFile("./texture/map/obstacle/box.png");
-                sprite.setTextureRect(sf::IntRect(0,0,100,100));
-                sprite.setOrigin(60,50);
-                area=sf::Vector2f(40,30);
-                collisionArea=sf::Vector2f(65,50);
-            }else{//open
-                open=true;
-                texture.loadFromFile("./texture/map/obstacle/box.png");
-                sprite.setTextureRect(sf::IntRect(118,0,100,100));
-                sprite.setOrigin(60,50);
-                area=sf::Vector2f(40,30);
-                collisionArea=sf::Vector2f(65,50);
-            }
 
-            break;
-        default:
-            sprite.setTextureRect(sf::IntRect(0,0,0,0));
-            area=sf::Vector2f(380,150);
-            collisionArea=sf::Vector2f(0,0);
+public:
+    explicit Obstacle(ObstacleType type,int select=0);
+    sf::Vector2f getArea(){
+        return area;
     }
-        sprite.setTexture(texture);
-}
-void Obstacle::Render(sf::RenderWindow &window) {
-    window.draw(sprite);
-}
+    sf::Vector2f getPosition(){
+        return position;
+    }
+    void setPosition(sf::Vector2f position){
+        this->position=position;
+        sprite.setPosition(position);
+    }
+    void Render(sf::RenderWindow& window);
+
+    sf::Vector2f getCollisionArea(){
+        return collisionArea;
+    }
+    sf::Vector2f getCenterPosition(){
+        return {position.x,position.y+area.y/2-getCollisionArea().y/2};
+    }
+    bool isUnlockable(){
+        return unlockable;
+    }
+    bool unlock(keyType type){
+        if(unlockable)
+            open=(lock==type);
+        return open;
+    }
+    ObstacleType getType(){
+        return type;
+    }
+
+    bool isOpen(){
+        return open;
+    }
+    std::string getAction(){
+        if(open)
+            return action;
+        return "";
+    }
+    void setAction(std::string string){
+        action=string;
+    }
+};
